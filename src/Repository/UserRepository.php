e11c670f0563ddc69a58570da45d8e6fe06d09b0
Alexis BONJOUR <alexis.bonjour@outlook.fr>
2020-01-14 10:02:54 +0100

    Manage user

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -19,6 +19,12 @@ class UserRepository extends ServiceEntityRepository
         parent::__construct($registry, User::class);
     }
 
+    public function format($user)
+    {
+        return ['id' => $user->getId()];
+    }
+
+
     // /**
     //  * @return User[] Returns an array of User objects
     //  */
